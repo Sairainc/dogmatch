daa0cb3a8ed5183e5c7accda9b2eae35ffe5e25c
ryoga520z-gmail.com <ryoga520z@gmail.com>
2025-05-10 19:53:15 +0900

    Google認証

--- app/actions.ts
+++ app/actions.ts
@@ -132,3 +132,21 @@ export const signOutAction = async () => {
   await supabase.auth.signOut();
   return redirect("/sign-in");
 };
+
+export const signInWithGoogleAction = async () => {
+  const supabase = await createClient();
+  const origin = (await headers()).get("origin");
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: `${origin}/auth/callback`,
+    },
+  });
+
+  if (error) {
+    return encodedRedirect("error", "/sign-up", error.message);
+  }
+
+  return redirect(data.url);
+};
