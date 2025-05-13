da5c5b6aa05c1f8d9e35b370bff882e0addf892d
ryoga520z-gmail.com <ryoga520z@gmail.com>
2025-05-13 13:51:18 +0900

    フォームの修正

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen bg-white py-10 relative">
+    <div className="min-h-screen bg-white py-10 pb-24 relative">
       {/* 装飾要素 */}
       <div className="absolute -top-20 -right-20 w-64 h-64 rounded-full bg-gray-100 opacity-50" />
       <div className="absolute top-1/3 -left-20 w-40 h-40 rounded-full bg-gray-100 opacity-30" />
